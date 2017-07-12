e793e61549bf27901d2377dd6e37957cac77abd1
Diamond705 <n.diamond.705@gmail.com>
2017-07-12 03:47:53 +0100

    profile welcome message

---
+++ resources/views/welcomemsg.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.master') 
+
+@section('content')
+
+<div class="container">
+
+    <div class="row">
+        <!--begin section_box -->
+        <div class="text-center">
+
+            <div class="section_title text-center">
+                <h2>You are now logged in!</h2>
+            </div>
+
+
+        </div>
+        <!--end section_box -->
+
+    </div>
+
+</div>
+
+@endsection
