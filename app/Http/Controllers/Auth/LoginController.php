04a76dafade92339530f5fd7a1bb9df5d47c6653
Diamond705 <n.diamond.705@gmail.com>
2017-07-12 04:02:12 +0100

    Login Controller Tweak

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/welcomemsg';
+    protected $redirectTo = '/welcome';
 
     /**
      * Create a new controller instance.
