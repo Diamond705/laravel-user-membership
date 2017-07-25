967f7910af3170a34714973a09afa2c05d95e980
Diamond705 <n.diamond.705@gmail.com>
2017-07-25 18:15:09 +0100

    suspended profile page display for social auth

--- resources/views/welcomemsg.blade.php
+++ resources/views/welcomemsg.blade.php
@@ -7,44 +7,10 @@
         <div class="row">
             <!--begin section_box -->
             <div class="text-center">
-                @if($provider == 'facebook')
-                    <div class="section_title text-center">
-                        <img src="{{ $details->user['avatar'] }}"
-                             style="width:150px; height:150px; border-radius:50%;">
-                        <h2>Welcome {{ $details->user['name']}} ! <br> Your email is : {{
-    $details->user['email'] }}.</h2>
-                    </div>
-
-                @elseif($provider == 'google')
-                    <div class="section_title text-center">
-                        <img src="{{ $details->user['avatar'] }}"
-                             style="width:150px; height:150px; border-radius:50%;">
-                        <h2>Welcome {{ $details->user['name']}} ! <br> Your email is : {{
-    $details->user['email'] }}.</h2>
-                    </div>
-
-                @elseif($provider == 'twitter')
-                    <div class="section_title text-center">
-                        <img src="{{ $details->user['avatar'] }}"
-                             style="width:150px; height:150px; border-radius:50%;">
-                        <h2>Welcome {{ $details->user['name']}} ! <br> Your email is : {{
-    $details->user['email'] }}.</h2>
-                    </div>
-
-                @elseif($provider == 'github')
-                    <div class="section_title text-center">
-                        <img src="{{ $details->user['avatar'] }}"
-                             style="width:150px; height:150px; border-radius:50%;">
-                        <h2>Welcome {{ $details->user['name']}} ! <br> Your email is : {{
-    $details->user['email'] }}.</h2>
-                    </div>
-
-                @else
 
                     <div class="section_title text-center">
                         <h2>You are now logged in!</h2>
                     </div>
-                @endif
 
 
             </div>
