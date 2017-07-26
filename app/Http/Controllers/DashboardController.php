1447a0e23fc8f7510763d551abe0be834e53dc5c
Diamond705 <n.diamond.705@gmail.com>
2017-07-26 04:17:01 +0100

    Added Avatar change functionality

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use Auth;
+use Image;
 
 class DashboardController extends Controller
 {
@@ -24,6 +27,20 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('welcomemsg');
+        return view('welcomemsg', array('user' => Auth::user()));
+    }
+
+    public function update_avatar(Request $request) {
+//        Handle Upload of avatar
+        if($request->hasFile('avatar')) {
+            $avatar = $request->file('avatar');
+            $filename = time() . '.' . $avatar->getClientOriginalExtension();
+            Image::make($avatar)->resize(300, 300)->save(public_path('/images/' . $filename));
+
+            $user = Auth::user();
+            $user->avatar = $filename;
+            $user->save();
+        }
+        return view('welcomemsg', array('user' => Auth::user()));
     }
 }
